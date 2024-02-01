6a5a02401169083c7a6cff6ce0fc70da22d4c169
Hamza <L1S21BSSE0018@ucp.com>
2024-02-01 11:57:19 +0500

    adding class code

---
+++ E-learning.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+class E-Learning{
+	private:
+	int id;
+	string course;
+	public:
+	E-Learning();
+	public void set_id(int id){};
+	public int get_id(){};
+	public void set_course(string course){};
+	public string get_course(){};
+	
+	
+	
+	
+	
+}
+int main(){
+	
+	
+	
+	return 0;
+}
